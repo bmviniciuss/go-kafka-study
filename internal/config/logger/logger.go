08247ac4788d6a85656b8ec02e567064fd1463eb
Vinicius Barbosa de Medeiros <bmvinicius11@gmail.com>
2024-03-05 23:27:08 -0300

    feat: add zap logger

---
+++ internal/config/logger/logger.go
@@ -0,0 +1,32 @@
+package logger
+
+import (
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
+)
+
+func NewLogger() *zap.SugaredLogger {
+	// Configure logger options
+	config := zap.Config{
+		Encoding:         "json",
+		Level:            zap.NewAtomicLevelAt(zapcore.InfoLevel),
+		OutputPaths:      []string{"stdout"},
+		ErrorOutputPaths: []string{"stderr"},
+		EncoderConfig: zapcore.EncoderConfig{
+			MessageKey:     "message",
+			LevelKey:       "level",
+			TimeKey:        "time",
+			EncodeLevel:    zapcore.CapitalLevelEncoder,
+			EncodeTime:     zapcore.ISO8601TimeEncoder,
+			EncodeDuration: zapcore.SecondsDurationEncoder,
+		},
+	}
+
+	// Create logger
+	logger, _ := config.Build()
+
+	// Create sugared logger
+	sugaredLogger := logger.Sugar()
+
+	return sugaredLogger
+}
